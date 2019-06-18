1612f3118c102d299dd400b0d6d0603db8d7caf6
Kevin Spies <kevin.spies@gmail.com>
2019-06-17 21:23:19 -0400

    beginning of skeleton and psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var question1 = "Who played Fezzik in 1987 The Princess Bride?";
+var question2 = "";
+
+
+//if selects correct answer
+    //correctAnswers++
+    //display yayVideo
+    //new question screen
+//else
+    //wrongAnswers++
+    //display wahVideo
+    //new question screen
