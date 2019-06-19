96454090b5ffca85fbd0e060a9163d3bd5a7df3b
Kevin Spies <kevin.spies@gmail.com>
2019-06-19 01:19:15 -0400

    added repopulate function and removed object in favor of an array to use with repopulate, my function to repopulate the buttons with new answers which i'll call when a new q/a is required

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,9 @@
 
-var set1 = { question: "Who played Fezzik in 1987 The Princess Bride?", correct: "Andre the Giant", wrong1: "Mike Wazowski", wrong2: "Jennifer Lopez", wrong3: "Michael Cera" };
-var set2 = { question: "Which of the following players has a two handed backhand?", wrong1: "Roger Federer", correct: "Novak Djokovic", wrong2: "Stan Wawrinka", wrong3: "Dominic Thiem" };
+var set1 = ["Who played Fezzik in 1987 The Princess Bride?", "Andre the Giant", "Mike Wazowski", "Jennifer Lopez", "Michael Cera"];
+var set2 = ["Which of the following players has a two handed backhand?", "Roger Federer", "Novak Djokovic", "Stan Wawrinka", "Dominic Thiem"];
 
 
 //-----------------------------one question loop-------------------------------
-// $("#question").text(have some smart adder in here that goes through the questions? array?)
 
 var timeLeft = 20;
 
@@ -13,48 +12,26 @@ setInterval(function () {
     timeLeft--;
 }, 1000);
 
-
-$(".question").click(function () {
+console.log("hey there");
+$(".answer").click(function () {
     console.log(this.value);
+    console.log("hello there");
     if (this.value === set1.correct) {//correct
         console.log("correct!");
     }
 });
 
-// if($(".question").on("click"))
-//if wrong answer or time up
-//wrongAnswers++
-//display wahVideo
-//new question screen
 
 
 
 //-----------------------------one question loop-------------------------------
 
 //make a function that loads in a new set - of a question and 4 possible answers
-//so i'll probably wanna save each set in an array, likely
 
-function loadSet1() {
-    $("#question").text(set1.question);
-    $("#answer1").text(set1.correct);
-    $("#answer2").text(set1.wrong1);
-    $("#answer3").text(set1.wrong2);
-    $("#answer4").text(set1.wrong3);
+function rePopulate(someArray) {
+    $("#question").text(someArray[0]);
+    $("#answer1").text(someArray[1]);
+    $("#answer2").text(someArray[2]);
+    $("#answer3").text(someArray[3]);
+    $("#answer4").text(someArray[4]);
 }
-function loadSet2() {
-    $("#question").text(set2.question);
-    $("#question").text(set2.wrong1);
-    $("#question").text(set1.correct);
-    $("#question").text(set1.wrong2);
-    $("#question").text(set1.wrong3);
-}
-
-loadSet1();
-//if selects correct answer
-    //correctAnswers++
-    //display yayVideo
-    //new question screen
-//else
-    //wrongAnswers++
-    //display wahVideo
-    //new question screen
