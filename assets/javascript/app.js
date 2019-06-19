c1805337049666be463b65b0b3117cdbc24e027b
Kevin Spies <kevin.spies@gmail.com>
2019-06-19 19:52:19 -0400

    jeezuss. i was referencing my image from the .js file instead of the html file.. ok, i can move on because i can now bring up images, well, i will make them videos soon ;D

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,38 @@
-
 var set1 = ["Who played Fezzik in 1987 The Princess Bride?", "Andre the Giant", "Mike Wazowski", "Jennifer Lopez", "Michael Cera"];
 var set2 = ["Which of the following players has a two handed backhand?", "Roger Federer", "Novak Djokovic", "Stan Wawrinka", "Dominic Thiem"];
 
+var correctAnswers = ["Andre the Giant", "Novak Djokovic"];
 
-//-----------------------------one question loop-------------------------------
-
+//-------------------------------one question loop-----------------------------------
 var timeLeft = 20;
+var timer = setInterval(tick(), 1000);
 
-setInterval(function () {
-    $("#timeremaining").text("Time Remaining: " + timeLeft);
-    timeLeft--;
-}, 1000);
-
-console.log("hey there");
-
-$("#answer1").on("click", console.log("click detected"));//not working?
-
-$(".answer").click(function () {//not working???
-    //console.log(this.value);
-    console.log("hello there");
-    if (this.value === set1.correct) {//correct
-        console.log("correct!");
-    }
+$(".answer").click(function () {//just testing
+    $("#image-holder").html("<img src=" + "assets/images/ninjaWrong.png" + " width='400px'>");
 });
 
+if (timeLeft === 0) {
+    clearInterval(timer);
+    alert("Too slow!");
+}
 
 
+//-----------------------------end of one question loop--------------------------------
 
-//-----------------------------one question loop-------------------------------
-
-//make a function that loads in a new set - of a question and 4 possible answers
-
+//------------------------------------functions----------------------------------------
+function tick() {
+    timeLeft--;
+    $("#timeremaining").text("Time Remaining: " + timeLeft);
+}
 function rePopulate(someArray) {
     $("#question").text(someArray[0]);
     $("#answer1").text(someArray[1]);
     $("#answer2").text(someArray[2]);
     $("#answer3").text(someArray[3]);
     $("#answer4").text(someArray[4]);
+}
+
+function isCorrect(someAnswer) {
+    if (correctAnswers.includes(someAnswer)) return true;
+    else return false;
 }
