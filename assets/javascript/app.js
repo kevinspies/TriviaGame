1b9a364d96b2a24738c1c84d54518240da5ab181
Kevin Spies <kevin.spies@gmail.com>
2019-06-21 18:34:00 -0400

    this should be global no?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,11 @@ var timer = setInterval(tick, 1000);
 var nextQuestion;
 
 
+//wait this information should be global... this ticker reset function...
+if (tick.timeLeft === 0) {
+    console.log("whoops! all out of time! and in a proper question 1 logic?!");
+    nextQuestion = setTimeout(rePopulate, 2000);
+}
 
 $(".answer").click(function () {
     // console.log(this.value);
@@ -30,13 +35,18 @@ $(".answer").click(function () {
     var guess = event.target.textContent; //this represents the string of their guess
     console.log(typeof (words));//string, well that's good.
 
+    //question 1
     if (guess === "green" || guess === "black" || guess === "white") {//then they guessed wrong
         console.log(event.target.textContent + " is incorrect!");
         $("#image-holder").html("<img src=" + "assets/images/ninjaWrong.png" + " width='400px'>");
         nextQuestion = setTimeout(rePopulate, 2000);
     }
+    else if (tick.timeLeft === 0) {
+        console.log("whoops! all out of time! and in a proper question 1 logic?!");
+        nextQuestion = setTimeout(rePopulate, 2000);
+    }
 
-    //next question
+    //question 2
     if (event.target.textContent === "Mike Wazowski" || event.target.textContent === "Jennifer Lopez" || event.target.textContent === "Michael Cera") {
         $("#image-holder").html("<img src=" + "assets/images/zapdos.jpg" + " width='400px'>");
         nextQuestion = setTimeout(rePopulate2, 2000);
@@ -54,7 +64,9 @@ function tick() {
     $("#timeremaining").text("Time Remaining: " + timeLeft);
     if (timeLeft === 0) {
         clearInterval(timer);
-        alert("Too slow!");
+        // alert("Too slow!");
+        //next question
+        //this could be problematic, how does tick know which question it is on?
     }
 }
 function rePopulate() {
@@ -62,6 +74,8 @@ function rePopulate() {
     timeLeft = 20;
     timer = setInterval(tick, 1000);
     //remove wrong ninja here at the start of a new question
+    $("#image-holder").html("<img src=");
+
     $("#question").attr(set1[0]);
     $("#answer1").text(set1[1]);
     $("#answer2").text(set1[2]);
@@ -69,7 +83,9 @@ function rePopulate() {
     $("#answer4").text(set1[4]);
 }
 function rePopulate2() {
-    //remove wrong ninja here at the start of a new question
+    timeLeft = 20;
+    timer = setInterval(tick, 1000);
+    $("#image-holder").html("<img src=");
     $("#question").attr(set2[0]);
     $("#answer1").text(set2[1]);
     $("#answer2").text(set2[2]);
