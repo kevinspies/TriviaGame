46c8cc20f516ae7a1dcd76951e24c06d9ce31ce0
Kevin Spies <kevin.spies@gmail.com>
2019-06-21 19:12:45 -0400

    added anySet as a parameter to repopulate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,11 +9,11 @@ var nextQuestion;
 
 //wait this information should be global... this ticker reset function...
 
-function outOfTime() {
+function outOfTime(specificIndex) {
     if (timeLeft === 0) {
         console.log("whoops! all out of time!");
         //here i could hard check to see which question i'm on by using jquery to check
-        nextQuestion = setTimeout(rePopulate, 2000);
+        nextQuestion = setTimeout(rePopulate(specificIndex), 2000);
     }//okay i need to be able to see where i am in an array of questions so i can always call out of time
 }//no matter where i am, so this means i need to be able to say repopulate next question
 //do i need a game object then? 
@@ -23,18 +23,14 @@ $(".answer").click(function () {
     // var title = $(this).text;
     // alert(title);
     clearInterval(timer);
-
-
     //sooo event returns a DOM element... so we use dom methods? and kinda works like this
     //inside an event
-
     //wrong answers
     // console.log("no way!!");
     // console.log("i can also get text like this maybe.. let's find out! button text isss... " + $(".button").text() + "w0wz3rzLol0c0pt3r");
     //no that did not work, is that because jquery ltext() method doesn't work with buttons?
     //or something to do with ID? this is definitely important. 
     //ok.. so i just did not realize how useful jquery was
-
     var guess = event.target.textContent; //this represents the string of their guess
     console.log(typeof (words));//string, well that's good.
 
@@ -55,7 +51,7 @@ $(".answer").click(function () {
     }
 
     //question 2
-    if (event.target.textContent === "Mike Wazowski" || event.target.textContent === "Jennifer Lopez" || event.target.textContent === "Michael Cera") {
+    if (guess === "Mike Wazowski" || guess === "Jennifer Lopez" || guess === "Michael Cera") {
         $("#image-holder").html("<img src=" + "assets/images/zapdos.jpg" + " width='400px'>");
         nextQuestion = setTimeout(rePopulate2, 2000);
     }
@@ -64,9 +60,7 @@ $(".answer").click(function () {
 
 });
 
-//-----------------------------end of one question loop--------------------------------
 
-//------------------------------------functions----------------------------------------
 function tick() {
     timeLeft--;
     $("#timeremaining").text("Time Remaining: " + timeLeft);
@@ -77,29 +71,31 @@ function tick() {
         //this could be problematic, how does tick know which question it is on?
     }
 }
-function rePopulate() {
+function rePopulate(anySet) {
     //whenever a new question comes in.. i need to start ticking down again
     timeLeft = 20;
     timer = setInterval(tick, 1000);
     //remove wrong ninja here at the start of a new question
     $("#image-holder").html("<img src=");
 
-    $("#question").attr(set1[0]);
-    $("#answer1").text(set1[1]);
-    $("#answer2").text(set1[2]);
-    $("#answer3").text(set1[3]);
-    $("#answer4").text(set1[4]);
+    $("#question").attr(anySet[0]);
+    $("#answer1").text(anySet[1]);
+    $("#answer2").text(anySet[2]);
+    $("#answer3").text(anySet[3]);
+    $("#answer4").text(anySet[4]);
 }
-function rePopulate2() {
-    timeLeft = 20;
-    timer = setInterval(tick, 1000);
-    $("#image-holder").html("<img src=");
-    $("#question").attr(set2[0]);
-    $("#answer1").text(set2[1]);
-    $("#answer2").text(set2[2]);
-    $("#answer3").text(set2[3]);
-    $("#answer4").text(set2[4]);
-}
+
+//i shouldn't need multiple repopulate functions now that i'm passing anySet as a parameter i think..
+// function rePopulate2() {
+//     timeLeft = 20;
+//     timer = setInterval(tick, 1000);
+//     $("#image-holder").html("<img src=");
+//     $("#question").attr(set2[0]);
+//     $("#answer1").text(set2[1]);
+//     $("#answer2").text(set2[2]);
+//     $("#answer3").text(set2[3]);
+//     $("#answer4").text(set2[4]);
+// }
 
 function isCorrect(someAnswer) {
     if (correctAnswers.includes(someAnswer)) return true;
