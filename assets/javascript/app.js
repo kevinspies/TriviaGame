0171820a4ac0270f35a265a30bcab883143bfbed
Kevin Spies <kevin.spies@gmail.com>
2019-06-21 17:49:45 -0400

    it is a string, weow, what are these files, weow

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,20 +17,29 @@ $(".answer").click(function () {
     clearInterval(timer);
 
 
-
-
-
     //sooo event returns a DOM element... so we use dom methods? and kinda works like this
     //inside an event
-    console.log(event.target.textContent);
 
-    if (event.target.textContent === "green" || event.target.textContent === "black" || event.target.textContent === "white") {
-        console.log("no way!!");
-        console.log("i can also get text like this maybe.. let's find out! button text isss... " + $(".button").text() + "w0wz3rzLol0c0pt3r");
-        //no that did not work, is that because jquery ltext() method doesn't work with buttons?
-        //or something to do with ID? this is definitely important. 
+    //wrong answers
+    // console.log("no way!!");
+    // console.log("i can also get text like this maybe.. let's find out! button text isss... " + $(".button").text() + "w0wz3rzLol0c0pt3r");
+    //no that did not work, is that because jquery ltext() method doesn't work with buttons?
+    //or something to do with ID? this is definitely important. 
+    //ok.. so i just did not realize how useful jquery was
+
+    var words = event.target.textContent;
+    console.log(typeof (words));//string, well that's good.
+
+    if (words === "green" || words === "black" || words === "white") {
+        console.log(event.target.textContent + " is correct!");
         $("#image-holder").html("<img src=" + "assets/images/ninjaWrong.png" + " width='400px'>");
         nextQuestion = setTimeout(rePopulate, 2000);
+    }
+
+    //next question
+    if (event.target.textContent === "Mike Wazowski" || event.target.textContent === "Jennifer Lopez" || event.target.textContent === "Michael Cera") {
+        $("#image-holder").html("<img src=" + "assets/images/zapdos.jpg" + " width='400px'>");
+        nextQuestion = setTimeout(rePopulate2, 2000);
     }
     //else if correct, show right ninja, repopulate, etc, etc 
 
@@ -55,6 +64,14 @@ function rePopulate() {
     $("#answer2").text(set1[2]);
     $("#answer3").text(set1[3]);
     $("#answer4").text(set1[4]);
+}
+function rePopulate2() {
+    //remove wrong ninja here at the start of a new question
+    $("#question").attr(set2[0]);
+    $("#answer1").text(set2[1]);
+    $("#answer2").text(set2[2]);
+    $("#answer3").text(set2[3]);
+    $("#answer4").text(set2[4]);
 }
 
 function isCorrect(someAnswer) {
